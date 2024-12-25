bafd9bafe71e54bf8758d8930e8edbfe08c9cbe1
dnyaneshwarpote <dnyaneshwarpote@yahoo.in>
2024-12-25 17:20:48 +0530

    removing extra committed files

--- src/main/java/com/org/mfm_bo/repository/StockTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.mfm_bo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.mfm_bo.entity.StockTransaction;
-
-public interface StockTransactionRepository extends JpaRepository<StockTransaction, Long> {
-
-}
