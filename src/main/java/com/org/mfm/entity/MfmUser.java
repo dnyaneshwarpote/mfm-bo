85a06ee0ba7ab85ce0c3b0c101fb2da47f1e9a35
dnyaneshwarpote <dnyaneshwarpote@yahoo.in>
2024-12-25 17:24:32 +0530

    entity tag was missiong

--- src/main/java/com/org/mfm/entity/MfmUser.java
+++ src/main/java/com/org/mfm/entity/MfmUser.java
@@ -1,10 +1,12 @@
 package com.org.mfm.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class MfmUser {
 	
 	@Id
