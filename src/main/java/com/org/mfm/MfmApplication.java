ba4dcbee5fa3a825159bee2b46ffe8103e0a0c05
dnyaneshwarpote <dnyaneshwarpote@yahoo.in>
2024-12-25 17:05:01 +0530

    reverting to java 17

--- src/main/java/com/org/mfm/MfmApplication.java
+++ src/main/java/com/org/mfm/MfmApplication.java
@@ -3,13 +3,11 @@ package com.org.mfm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class MfmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MfmApplication.class, args);
 	}
-
 
 }
